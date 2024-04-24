cccd9df650213b3298bc9f1ed19065c543b0cfde
Paulocdcm <paulo.cesarcruz2004@gmail.com>
2024-04-24 15:06:21 -0400

    Desafio 01

--- Desafio01RankJS.js
+++ Desafio01RankJS.js
@@ -3,28 +3,20 @@ let xp = 8050;
 let rank
 
 if (xp < 1001) {
-    console.log("Ferro");
     rank= "ferro"
 } else if (xp > 1000 && xp < 2001) {
-    console.log("Bronze");
     rank= "bronze"
 } else if (xp > 2000 && xp < 6001) {
-    console.log("Prata");
     rank="Prata"
 } else if (xp > 6000 && xp < 7001) {
-    console.log("Ouro");
     rank="Ouro"
 } else if (xp > 7000 && xp < 8001) {
-    console.log("Platina");
     rank="Platina"
 } else if (xp > 8000 && xp < 9001) {
-    console.log("Ascendente");
     rank="Ascendente"
 } else if (xp > 9000 && xp < 10001) {
-    console.log("Imortal");
     rank="Imortal"
 } else {
-    console.log("Radiante");
     rank="Radiante"
 }
 
